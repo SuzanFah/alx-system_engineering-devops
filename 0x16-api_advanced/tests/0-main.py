b3f764fefc52eb6579f6d58a288127c41dfc91fa
SuzanFah <susiefahim@gmail.com>
2024-08-14 03:23:55 +0300

    main0

--- 0x16-api_advanced/tests/0-main.py
+++ 0x16-api_advanced/tests/0-main.py
@@ -1,10 +1,23 @@
 #!/usr/bin/python3
+"""
+0-main
+"""
 import sys
-
+import importlib.util
 
 if __name__ == '__main__':
-    number_of_subscribers = __import__('0-subs').number_of_subscribers
+    # Load the 0-subs module dynamically
+    module_name = '0-subs'
+    module_path = f'{module_name}.py'
+
+    spec = importlib.util.spec_from_file_location(module_name, module_path)
+    module = importlib.util.module_from_spec(spec)
+    sys.modules[module_name] = module
+    spec.loader.exec_module(module)
+
+    number_of_subscribers = module.number_of_subscribers
+
     if len(sys.argv) < 2:
         print("Please pass an argument for the subreddit to search.")
     else:
-        print("{:d}".format(number_of_subscribers(sys.argv[1])))
+        print(number_of_subscribers(sys.argv[1]))
