98f35ac3c7ee1faf74c3bebd710728da4ffd6311
SuzanFah <susiefahim@gmail.com>
2024-08-14 05:44:18 +0300

    fixing

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -1,5 +1,6 @@
-# Example Puppet manifest for Windows
-exec { 'Fix_wordpress_site':
-  command => 'powershell.exe -Command "Write-Output \'Fixing WordPress site...\'"',
-  path    => ['C:/Windows/System32', 'C:/Windows'],
+# Fixes bad `phpp` extensions to `php` in the WordPress file `wp-settings.php`.
+
+exec { 'fix-wordpress':
+  command => 'sed -i s/phpp/php/g /var/www/html/wp-settings.php',
+  path    => '/usr/local/bin/:/bin/'
 }
