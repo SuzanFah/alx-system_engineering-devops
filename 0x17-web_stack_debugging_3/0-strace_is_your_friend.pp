80ddf45080bfc4b5d5fcdd557075b060070e0215
SuzanFah <susiefahim@gmail.com>
2024-08-15 13:06:27 +0000

    ruby

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -1,5 +1,29 @@
-exec { 'Fix wordpress site':
-  command   => 'sed -i "s/oldtext/newtext/" /c/Apache24/htdocs/wordpress/wp-config.php',
-  provider  => 'shell',
-  onlyif    => 'test -f /c/Apache24/htdocs/wordpress/wp-config.php',
+# This Puppet script fixes Apache configuration errors identified using strace.
+
+# Ensure that the Apache configuration directory exists
+file { '/etc/apache2':
+  ensure => directory,
+}
+
+# Ensure that the web root directory exists and has correct permissions
+file { '/var/www/html':
+  ensure  => directory,
+  owner   => 'www-data',
+  group   => 'www-data',
+  mode    => '0755',
+}
+
+# Create the missing wp-settings.php file
+file { '/var/www/html/wp-settings.php':
+  ensure  => file,
+  owner   => 'www-data',
+  group   => 'www-data',
+  mode    => '0644',
+  content => '/* Configuration settings */',
+}
+
+# Ensure Apache service is running
+service { 'apache2':
+  ensure => running,
+  enable => true,
 }
