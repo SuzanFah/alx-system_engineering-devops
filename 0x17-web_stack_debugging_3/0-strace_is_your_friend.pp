d3144b2ff07678c56761b3264444031bb65bea24
SuzanFah <138693557+SuzanFah@users.noreply.github.com>
2024-08-17 19:12:29 +0300

    Delete 0x17-web_stack_debugging_3 directory

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++
@@ -1,6 +0,0 @@
-# Fixes bad "phpp" extensions to "php" in "wp-settings.php"
-
-exec{'Fix-wordpress]':
-  command  => 'sed -i s/phpp/php/g /var/www/html/wp-settings.php',
-  path     => '/usr/local/bin/:/bin/'
-}
